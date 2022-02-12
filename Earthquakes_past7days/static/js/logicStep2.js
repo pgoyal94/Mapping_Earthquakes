0a02e62f11fe5b6aefae5fb19aeedf4cd8418196
Parul Goyal <parul.goyal@generatecapital.com>
2022-02-12 11:43:25 -0800

    Adding step 2 of Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -1,4 +1,4 @@
-// Step 1 - Create a map with all recorded earthquakes from the past 7 days.
+// Step 2 - Add style to the earthquake data
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -30,16 +30,48 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+// // Create a style for the lines.
+// let myStyle = {
+//   color: "black",
+//   weight: 1,
+//   fillColor: "orange"
+// }
+
 // Grabbing our GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   console.log(data);
+  // This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into a function to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  // This function determines the radius of the earthquake marker based on its magnitude. Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    // style: myStyle,
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      // layer.bindPopup("<h3>" +"Neighborhood: " + feature.properties.AREA_NAME + "</h3>" );
-    }
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+    console.log(data);
+    return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // onEachFeature: function(feature, layer) {
+    //   console.log(layer);
+    //   // layer.bindPopup("<h3>" +"Neighborhood: " + feature.properties.AREA_NAME + "</h3>" );
+    // }
     
   }).addTo(map);
 });
